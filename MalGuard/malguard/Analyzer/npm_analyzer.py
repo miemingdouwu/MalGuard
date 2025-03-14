89288bbfd224cd5c8e4a80fefaa8f421c23c1846
miemingdouwu <1970870256@qq.com>
2025-03-14 22:56:59 +0800

    MalGuard分析器

--- MalGuard/malguard/Analyzer/npm_analyzer.py
+++ MalGuard/malguard/Analyzer/npm_analyzer.py
@@ -6,34 +6,34 @@ import multiprocessing
 from functools import partial
 from openai import OpenAI
 
-#文件当前路径
+# 文件当前路径
 current_dir = os.path.dirname(os.path.abspath(__file__))
-#上一级目录
+# 上一级目录
 parent_dir = os.path.dirname(current_dir)
 
 # 配置日志
 logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(processName)s - %(levelname)s - %(message)s',
-    filemode='a'
 )
 logger = logging.getLogger()
+
 
 def init_worker():
     """子进程日志初始化"""
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(processName)s - %(levelname)s - %(message)s',
-        filemode='a'
     )
 
-def process_single_file(file_path, model, api_key, base_url):
+
+def process_single_file(file_path, output_dir, api_key, base_url, model):
     """处理单个文件的包装函数"""
     local_logger = logging.getLogger()
     local_logger.info(f"开始处理文件: {file_path}")
     try:
-        analyzer = NPMAnalyzer(api_key, base_url)
-        result = analyzer.llm_evaluate(model, file_path)
+        analyzer = NPMAnalyzer(api_key, base_url, model)
+        result = analyzer.llm_evaluate(file_path, output_dir)
         if result:
             tag, filename = result
             local_logger.info(f"文件 {file_path} 处理完成")
@@ -43,12 +43,14 @@ def process_single_file(file_path, model, api_key, base_url):
         local_logger.error(f"处理文件 {file_path} 失败: {str(e)}", exc_info=True)
         return None
 
-class NPMAnalyzer:
-    def __init__(self, api_key, base_url):
-        self.api_key=api_key
-        self.base_url=base_url
 
-    def llm_conversation(self, model, system_prompt, user_prompt):
+class NPMAnalyzer:
+    def __init__(self, api_key, base_url, model):
+        self.api_key = api_key
+        self.base_url = base_url
+        self.model = model
+
+    def llm_conversation(self, system_prompt, user_prompt):
         """
         system_prompt：系统提示词
         user_prompt：用户提示词
@@ -56,12 +58,12 @@ class NPMAnalyzer:
         """
         # 生成评估报告
         client = OpenAI(
-            api_key = self.api_key,
-            base_url = self.base_url
+            api_key=self.api_key,
+            base_url=self.base_url
         )
 
         evaluate_completion = client.chat.completions.create(
-            model=model,
+            model=self.model,
             messages=[
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": user_prompt}
@@ -100,7 +102,6 @@ class NPMAnalyzer:
         return：包名，扫描结果，脚本文件，其他文件
         """
         filename, scan_result = self.read_scan_result(scan_result_path)
-
 
         if scan_result["issues"] == 0:
             package_path = os.path.join(parent_dir, 'detection/npm', str(filename))
@@ -147,14 +148,11 @@ class NPMAnalyzer:
         else:
             return filename, scan_result, None, None
 
-    def llm_evaluate(self, model, scan_result_path):
+    def llm_evaluate(self, scan_result_path, output_dir):
         """
-        model：使用模型
         scan_result_path：扫描结果文件路径
         return：标签，包名
         """
-        if self.analyze_scan_result(scan_result_path) is None:
-            return
 
         tag = None
 
@@ -186,29 +184,27 @@ class NPMAnalyzer:
         # print(user_prompt)
         # print("___________________________________________________________________________________________")
 
-        report_path = os.path.join(parent_dir, 'evaluate_reports/npm', str(filename) + '.json')
+        report_path = os.path.join(output_dir, str(filename) + '.json')
 
         if os.path.exists(report_path):
             return tag, filename
 
         if len(user_prompt) > 20000:
-            user_prompt = user_prompt[:20000] + '"' +'}'
+            user_prompt = user_prompt[:20000] + '"' + '}'
 
-        report = self.llm_conversation(model, system_prompt, user_prompt)
+        report = self.llm_conversation(system_prompt, user_prompt)
 
         if report.startswith('```json'):
             report = report[len('```json'):]
         if report.endswith('```'):
             report = report[:-len('```')]
 
-
         # print("___________________________________________________________________________________________")
         # print(report)
         # print("___________________________________________________________________________________________")
 
-        evaluate_reports_path = os.path.join(parent_dir, 'evaluate_reports/npm')
-        if not os.path.exists(evaluate_reports_path):
-            os.makedirs(evaluate_reports_path)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
 
         with open(report_path, 'w', encoding='utf-8') as f:
             f.write(report)
@@ -249,7 +245,7 @@ class NPMAnalyzer:
     #         for item in has_issues_list:
     #             writer.writerow([item])
 
-    def llm_evaluate_all(self, model, scan_result_folder_path):
+    def llm_evaluate_all(self, scan_result_folder_path, evaluate_report_folder_path):
         """多进程处理所有扫描结果"""
         logger.info(f"开始扫描目录: {scan_result_folder_path}")
 
@@ -270,7 +266,8 @@ class NPMAnalyzer:
         # 使用partial固定参数
         process_func = partial(
             process_single_file,
-            model=model,
+            output_dir=evaluate_report_folder_path,
+            model=self.model,
             api_key=self.api_key,
             base_url=self.base_url
         )
@@ -298,16 +295,110 @@ class NPMAnalyzer:
                 has_issues_list.append(filename)
 
         # 写入结果文件
-        npm_no_issues_list_path = os.path.join(parent_dir, 'evaluate_reports/npm_no_issues_list.csv')
+        npm_no_issues_list_path = os.path.join(evaluate_report_folder_path, '../npm_no_issues_list.csv')
         with open(npm_no_issues_list_path, 'w', newline='', encoding='utf-8') as f:
             writer = csv.writer(f)
             writer.writerows([[name] for name in no_issues_list])
 
-        npm_has_issues_list_path = os.path.join(parent_dir, 'evaluate_reports/npm_has_issues_list.csv')
+        npm_has_issues_list_path = os.path.join(evaluate_report_folder_path, '../npm_has_issues_list.csv')
         with open(npm_has_issues_list_path, 'w', newline='', encoding='utf-8') as f:
             writer = csv.writer(f)
             writer.writerows([[name] for name in has_issues_list])
 
         logger.info(f"处理完成！零事件包: {len(no_issues_list)} 个，风险包: {len(has_issues_list)} 个")
         return no_issues_list, has_issues_list
+
+
+    def llm_evaluate_demo(self, package_path, scan_result_path):
+        """
+        scan_result_path：扫描结果文件路径
+        return：标签，包名
+        """
+
+        with open(scan_result_path, 'r', encoding='utf-8') as file:
+            # 将json文件内容加载到字典中
+            scan_result = json.load(file)
+
+
+        # 用于存储所有Python文件内容的字典，键为文件名，值为文件内容字符串
+        index_js_and_package_json_content = {}
+        others_content = {}
+
+        if scan_result["issues"] == 0:
+            for root, dirs, files in os.walk(package_path):
+                for file in files:
+                    if file == 'package.json':
+                        file_path = os.path.join(root, file)
+                        try:
+                            # 读取文件内容并存储为字符串
+                            with open(file_path, 'r', encoding='utf-8') as f:
+                                content = json.load(f)
+                                index_js_and_package_json_content[file] = str(content)
+                        except:
+                            continue
+                    elif file == 'index.js':
+                        file_path = os.path.join(root, file)
+                        try:
+                            # 读取文件内容并存储为字符串
+                            with open(file_path, 'r', encoding='utf-8') as f:
+                                content = f.read()
+                                index_js_and_package_json_content[file] = str(content)
+                        except:
+                            continue
+                    elif file.endswith('.js') or file.endswith('.ts'):
+                        file_path = os.path.join(root, file)
+                        try:
+                            # 读取文件内容并存储为字符串
+                            with open(file_path, 'r', encoding='utf-8') as f:
+                                content = f.read()
+                                others_content[file] = str(content)
+                        except:
+                            continue
+                    else:
+                        continue
+
+        system_prompt = ""
+        user_prompt = ""
+
+        prompt_path = os.path.join(current_dir, 'npm_prompt.json')
+        with open(prompt_path, 'r', encoding='utf-8') as f:
+            system_prompt = str(json.load(f)['prompt'])
+
+        if index_js_and_package_json_content:
+            user_prompt = str(index_js_and_package_json_content)
+        elif others_content:
+            user_prompt = str(others_content)
+        else:
+            temp_dict = {}
+            temp_dict['guarddog_scan_result'] = str(scan_result)
+            user_prompt = str(temp_dict)
+
+        if len(user_prompt) > 20000:
+            user_prompt = user_prompt[:20000] + '"' + '}'
+
+        report = self.llm_conversation(system_prompt, user_prompt)
+
+        if report.startswith('```json'):
+            report = report[len('```json'):]
+        if report.endswith('```'):
+            report = report[:-len('```')]
+
+        report_path = './npm_analyzer_report.json'
+
+        with open(report_path, 'w', encoding='utf-8') as f:
+            f.write(report)
+
+        return report
+
+
+
+
+
+
+
+
+
+
+
+
 
